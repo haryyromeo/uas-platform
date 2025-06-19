a8f843e6cf033a7ca5cecfd862f4cffb142fdf4a
Nama Anda <akunsibudi@gmail.com>
2025-06-19 21:14:14 +0700

    Initial Spring Boot project

---
+++ src/main/java/com/uas/alat_musik_app/AlatMusikAppApplication.java
@@ -0,0 +1,13 @@
+package com.uas.alat_musik_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlatMusikAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlatMusikAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uas/alat_musik_app/AlatMusikAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uas.alat_musik_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlatMusikAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
